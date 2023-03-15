1f10c440d8f798249a0edbea497b7b0704733305
Norton <marcusquist@hotmail.dk>
2023-03-15 16:11:25 +0100

    added getMessages

--- build/index.js
+++ build/index.js
@@ -1,16 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const express = require('express');
+const express = require("express");
+var bodyParser = require("body-parser");
 const app = express();
 const port = 80;
 const isNosql = true;
-app.post('/saveMessage', (req, res) => {
-    console.log("REQ", req.example);
+app.use(bodyParser.json());
+app.post("/saveMessage", (req, res) => {
+    const message = req.body;
+    console.log("REQ", message);
     if (isNosql) {
     }
     else {
     }
-    res.send('Express + TypeScript Server');
+    res.send("Express + TypeScript Server");
+});
+app.get("/getMessage/:id", (req, res) => {
+    const chatroomID = req.params.id;
+    console.log("REQ", chatroomID);
+    if (isNosql) {
+    }
+    else {
+    }
+    res.send("Express + TypeScript Server");
 });
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
